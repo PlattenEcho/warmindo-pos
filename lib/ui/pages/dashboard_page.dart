8f05cca0e1385f0799449d8868b875ef59a951db
PlattenEcho <akunsekolahdika@gmail.com>
2023-12-20 16:24:37 +0700

    implement dashboard ui

--- lib/ui/pages/dashboard_page.dart
+++ lib/ui/pages/dashboard_page.dart
@@ -10,33 +10,125 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  String selectedWarung = 'Warung A';
+  List<String> warungList = ['Warung 1', 'Warung 2', 'Warung 3', 'Warung 4'];
+
   @override
   Widget build(BuildContext context) {
     return Container(
+        color: kWhiteColor,
         margin: const EdgeInsets.only(top: 5, left: 15, right: 15),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "Selamat Datang, User!",
-              style: blackTextStyle.copyWith(fontWeight: bold, fontSize: 21),
+            DropdownButtonFormField<String>(
+              focusColor: kGreenColor,
+              value: selectedWarung,
+              isExpanded: true,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedWarung = newValue!;
+                });
+              },
+              style: blackTextStyle.copyWith(fontSize: 16),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide:
+                        BorderSide(color: kBlackColor.withOpacity(0.2))),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide:
+                      BorderSide(color: kGreenColor), // Warna fokus hijau
+                ),
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              ),
+              icon: Icon(Icons.arrow_drop_down,
+                  color: kGreenColor.withOpacity(0.8)), // Ikona dropdown
+              items: warungList.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
-            gapH24,
+            gapH12,
             Container(
-              padding: const EdgeInsets.only(top: 5, left: 15, right: 15),
-              height: 30,
+              padding: const EdgeInsets.only(
+                  top: 15, left: 15, right: 15, bottom: 15),
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
-                  color: kGreenColor, borderRadius: BorderRadius.circular(5)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 3,
+                      blurRadius: 5,
+                      offset: Offset(0, 5), // changes position of shadow
+                    ),
+                  ],
+                  color: const Color.fromARGB(255, 249, 249, 249),
+                  borderRadius: BorderRadius.circular(10)),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     "Warung 1",
                     style:
-                        blackTextStyle.copyWith(fontWeight: bold, fontSize: 17),
+                        greenTextStyle.copyWith(fontWeight: bold, fontSize: 22),
                   ),
-                  Container()
+                  gapH12,
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image(
+                        fit: BoxFit.cover,
+                        height: MediaQuery.of(context).size.height / 4,
+                        width: MediaQuery.of(context).size.width,
+                        image: AssetImage('assets/bg_start.png')),
+                  ),
+                  gapH12,
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: kGreenColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Transaksi",
+                              style: whiteTextStyle.copyWith(
+                                  fontWeight: regular, fontSize: 14),
+                            ),
+                            Text(
+                              "Rp. 1.000.000,00",
+                              style: whiteTextStyle.copyWith(
+                                  fontWeight: bold, fontSize: 20),
+                            )
+                          ],
+                        ),
+                      ),
+                      gapW12,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Shift",
+                            style: greenTextStyle.copyWith(
+                                fontWeight: regular, fontSize: 14),
+                          ),
+                          Text(
+                            "1",
+                            style: greenTextStyle.copyWith(
+                                fontWeight: bold, fontSize: 20),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
